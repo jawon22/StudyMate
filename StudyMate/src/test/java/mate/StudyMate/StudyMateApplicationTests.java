556f51a6dff5489d5d9fb86fb5cbd06f540ae031
jawon22 <winwkdnjs@naver.com>
2025-03-10 01:56:00 +0900

    first commit

---
+++ StudyMate/src/test/java/mate/StudyMate/StudyMateApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.StudyMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
